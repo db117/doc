0f2a929458df6c33e10d37f24af80076de50cb61
在那遥远的瞬间 <z351622948@163.com>
2019-08-27 10:50:38 +0800

    开启编辑

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,8 +5,16 @@ module.exports = {
     title: '个人主页',
     description: '文档',
     themeConfig: {
+        repo: 'db117/doc',
+        // 你的 Git 项目地址，添加后会在导航栏的最后追加
+        // 启用编辑
+        editLinks: true,
+        // 编辑按钮的 Text
+        editLinkText: '编辑文档！',
+        // 编辑文档的所在目录
+        docsDir: 'docs',
         nav: navConf,
-        sidebar:sidebarConf,
+        sidebar: sidebarConf,
         lastUpdated: '上次更新',
     }
 }
---
+++ docs/README.md
@@ -0,0 +1,32 @@
+# doc
+
+### docker
+
+​    记录docker的一些操作
+
+## 工具
+### git
+​   git简单使用
+
+## java
+​   java里面的一下东西
+
+### linux
+
+​    linux通用的一些操作
+
+### centos
+
+​    centos特有
+
+### Ubuntu
+
+​    Ubuntu，deepin等通用
+
+### mysql
+
+​    mysql安装，操作等细节
+
+### Windows
+
+​    Windows下一下东西
