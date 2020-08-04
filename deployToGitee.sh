f2e1a27d4d1901ec8573978505cfd8ce90cc1063
在那遥远的瞬间 <z351622948@163.com>
2020-08-04 16:41:26 +0800

    deployToGitee

--- deployToGitee.sh
+++ deployToGitee.sh
@@ -3,10 +3,9 @@
 cd docs/.vuepress/dist
 git config user.name "在那遥远的瞬间"
 git config user.email "z351622948@163.com"
-git config user.password "$giteePassword"
+git config user.password $1
 git init
 git add .
 git commit -m "发布pages"
 git remote add origin https://gitee.com/db117/db117.git
 git push --force origin master:master
-cd ..
