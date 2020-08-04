565c06e96ef49710643dccef10aec5eff507fcc8
在那遥远的瞬间 <z351622948@163.com>
2020-08-04 15:52:43 +0800

    编译后同步部署到gitee

---
+++ deployToGitee.sh
@@ -0,0 +1,12 @@
+#代码在Travis中编译完成后，顺便部署到gitee pages中
+#!/bin/bash
+cd docs/.vuepress/dist
+git config user.name "在那遥远的瞬间"
+git config user.email "z351622948@163.com"
+git config user.password "$giteePassword"
+git init
+git add .
+git commit -m "发布pages"
+git remote add origin https://gitee.com/db117/db117.git
+git push --force origin master:master
+cd ..
