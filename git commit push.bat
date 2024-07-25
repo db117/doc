8e2fa9a96fa32df092caa03bc34b7f3d2063ebb7
db117 <z351622948@163.com>
2024-07-25 11:20:23 +0800

    git 脚本

--- git commit push.bat
+++ git commit push.bat
@@ -1,10 +1,13 @@
+@echo off
+chcp 65001 >nul
+
 git --version >nul 2>&1
 if %errorlevel% neq 0 (
     echo Git 未安装或未配置为全局可用。
     exit /b %errorlevel%
 )
 
-@echo off
+
 setlocal
 
 REM 提示用户输入提交信息
@@ -18,6 +21,13 @@ if "%commit_message%"=="" (
 
 REM 打印提交信息以确认
 echo 你的提交信息是: %commit_message%
+
+REM 检查 Git 是否可用
+git --version >nul 2>&1
+if %errorlevel% neq 0 (
+    echo Git 未安装或未配置为全局可用。
+    exit /b %errorlevel%
+)
 
 REM 执行 git add .
 git add .
@@ -35,6 +45,13 @@ if %errorlevel% neq 0 (
     exit /b %errorlevel%
 )
 
-echo git commit 成功，提交信息：%commit_message%
+REM 执行 git push
+git push
+if %errorlevel% neq 0 (
+    echo git push 失败。
+    exit /b %errorlevel%
+)
+
+echo git commit 和 git push 成功，提交信息：%commit_message%
 endlocal
 exit /b 0
