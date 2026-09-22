fed0e7e59e7886aedbbdb0dc33c2519d291c8d5d
db117 <z351622948@163.com>
2026-09-22 11:28:12 +0800

    Remove Baidu analytics script from site head

--- docs/.vitepress/config/head.ts
+++ docs/.vitepress/config/head.ts
@@ -2,15 +2,6 @@ import type { HeadConfig } from 'vitepress'
 
 const head: HeadConfig[] = [
     ['link', {rel: 'icon', href: '/favicon.ico'}],
-    ['script', {}, `
-            var _hmt = _hmt || [];
-            (function() {
-            var hm = document.createElement("script");
-            hm.src = "https://hm.baidu.com/hm.js?32d81e1cc669a3aef7da8deccdd959ef";
-            var s = document.getElementsByTagName("script")[0]; 
-            s.parentNode.insertBefore(hm, s);
-            })();
-        `]
 ];
 
 export default head
